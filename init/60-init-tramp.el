6e4375be7ba981580c728d9f078225141a42556b
Ruizhe Zhao <ruizhe_zhao@126.com>
2018-08-10 14:20:52 +0800

    add: add configuration about tramp.

---
+++ init/60-init-tramp.el
@@ -0,0 +1,12 @@
+
+(defconst putty-directory "C:/Users/Administrator/Desktop/lab/bin/putty")
+
+(when (eq window-system 'w32)
+  (setq tramp-default-method "plink")
+  (setq tramp-default-user "ruizhe")
+  (setq tramp-default-host "172.28.14.39")
+;;  (setq tram-default-host-alist '(("plink" nil "172.28.14.39")))
+  (when (and (not (string-match putty-directory (getenv "PATH")))
+	     (file-directory-p putty-directory))
+    (setenv "PATH" (concat putty-directory ";" (getenv "PATH")))
+    (add-to-list 'exec-path putty-directory)))
