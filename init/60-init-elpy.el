df763945335e42da1e330ffacc2026fad407d023
Ruizhe Zhao <ruizhe_zhao@126.com>
2018-09-05 14:12:34 +0800

    add: add flycheck

--- init/60-init-elpy.el
+++ init/60-init-elpy.el
@@ -1,7 +1,7 @@
 (package-require 'elpy)
 
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-module))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
-
 (elpy-enable)
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
