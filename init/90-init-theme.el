4ec97bd00d4d9aa4e511b45955eeed4e93300f52
Ruizhe Zhao <ruizhe_zhao@126.com>
2018-09-05 10:26:04 +0800

    change: change ative theme to 'material'

--- init/90-init-theme.el
+++ init/90-init-theme.el
@@ -1,5 +1,3 @@
 (when (display-graphic-p)
-  ;; (add-to-list 'custom-theme-load-path "~/.emacs.d/addons/atom-one-dark-theme")
-  ;; (load-theme 'atom-one-dark t))
-  (package-require 'material-theme)
-  (load-theme 'material t))
+  (add-to-list 'custom-theme-load-path "~/.emacs.d/addons/atom-one-dark-theme")
+  (load-theme 'atom-one-dark t))
