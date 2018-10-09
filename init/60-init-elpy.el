8d054a05e7b271367d99f41562e411f16d690a75
Ruizhe Zhao <ruizhe.zhao.g@gmail.com>
2018-10-09 22:30:10 +0800

    fix: optimize elpy mode

--- init/60-init-elpy.el
+++ init/60-init-elpy.el
@@ -1,7 +1,11 @@
-(package-require 'elpy)
 
-(elpy-enable)
+(defun pyhook ()
+  (package-require 'elpy)
+ (elpy-enable)
+  (elpy-mode)
+  (when (require 'flycheck nil t)
+    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+    (add-hook 'elpy-mode-hook 'flycheck-mode))
+  )
 
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+(add-hook 'python-mode-hook 'pyhook)
