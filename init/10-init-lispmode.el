2403471277259c9c65f0f0fb965205626825e62f
Ruizhe Zhao <ruizhe_zhao@126.com>
2018-10-19 18:43:16 +0800

    fix: rename some init's

--- init/10-init-lispmode.el
+++
@@ -1,4 +0,0 @@
-  ;; for convenient
-  (global-set-key (kbd "C-h C-f") 'find-function)
-  (global-set-key (kbd "C-h C-v") 'find-variable)
-  (global-set-key (kbd "C-h C-k") 'find-function-on-key)
---
+++ init/60-init-lispmode.el
@@ -0,0 +1,8 @@
+
+(defun zz/lisp-mode-hook ()
+	    (global-set-key (kbd "C-h C-f") 'find-function)
+	    (global-set-key (kbd "C-h C-v") 'find-variable)
+	    (global-set-key (kbd "C-h C-k") 'find-function-on-key))
+
+(add-hook 'lisp-mode-hook 'zz/lisp-mode-hook)
+(add-hook 'lisp-interaction-mode-hook 'zz/lisp-mode-hook)
