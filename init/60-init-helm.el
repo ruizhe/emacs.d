5969cba150289b88411de396d2196ba5def8309f
Ruizhe Zhao <ruizhe_zhao@126.com>
2018-11-12 17:04:32 +0800

    fix: optimize helm config

--- init/60-init-helm.el
+++ init/60-init-helm.el
@@ -1,2 +1,26 @@
 (use-package helm
-  :ensure t)
+  :ensure t
+  :bind (("M-x" . helm-M-x)
+	 ("C-x C-f" . helm-find-files)
+	 ("C-x b" . helm-buffers-list))
+  :bind (:map helm-map
+	      ("<tab>" . 'helm-execute-persistent-action)
+	      ("C-z" . 'helm-select-action))
+  :init (setq helm-M-x-fuzzy-match t
+	      helm-mode-fuzzy-match t
+	      helm-buffers-fuzzy-matching t
+	      helm-recentf-fuzzy-match t
+	      helm-locate-fuzzy-match t
+	      helm-semantic-fuzzy-match t
+	      helm-imenu-fuzzy-match t
+	      helm-completion-in-region-fuzzy-match t
+	      helm-candidate-number-list 150
+	      helm-split-window-in-side-p t
+	      helm-move-to-line-cycle-in-source t
+	      helm-echo-input-in-header-line t
+	      helm-autoresize-max-height 0
+	      helm-autoresize-min-height 20)
+  :config (progn
+	    (require 'helm-config)
+	    (helm-mode t))
+  )
