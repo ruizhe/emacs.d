739ce42ce0ac2f58871d7e91e65fc015e0a5fbbb
Ruizhe Zhao <ruizhe_zhao@126.com>
2018-12-06 10:09:32 +0800

    fix: make cnfonts profiles are suitable for different os

--- init/60-init-cnfonts.el
+++ init/60-init-cnfonts.el
@@ -2,5 +2,8 @@
   :ensure t
   :config (progn
 	    (cnfonts-enable)
-	    (cnfonts-set-spacemacs-fallback-fonts))
+	    (cnfonts-set-spacemacs-fallback-fonts)
+	    (setq cnfonts-use-system-type t)
+	    (setq cnfonts-profiles
+		  '("program" "org-mode" "read-book")))
   )
