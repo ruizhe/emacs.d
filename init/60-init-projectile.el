eb8ee664430069ff0ebcfaa5dca40fa99d9c41c9
Ruizhe Zhao <ruizhe.zhao.g@gmail.com>
2019-06-18 20:21:41 +0800

    fix: [projectile] use default key binding.

--- init/60-init-projectile.el
+++ init/60-init-projectile.el
@@ -2,5 +2,7 @@
   :ensure t
   :config (progn
 	    (projectile-global-mode t)
+	    (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 	    (setq projectile-indexing-method 'alien)
+	    (setq projectile-completion-system 'ivy)
 	    (setq projectile-svn-command "svn list -R . | grep -v '$/' | tr '\\n\\r' '\\0\\0'")))
