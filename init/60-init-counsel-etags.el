578a12dc551b2640867865458fa4d035ea82d128
Ruizhe Zhao <ruizhe.zhao.g@gmail.com>
2019-07-01 15:08:22 +0800

    fix: fix some small mistakes.

--- init/60-init-counsel-etags.el
+++ init/60-init-counsel-etags.el
@@ -1,11 +1,10 @@
 (use-package counsel-etags
   :ensure t
-  :hook ((prog-mode) . counsel-etags-mode)
   :bind (("M-l" . counsel-etags-list-tag)
-	 ("M-," . counsel-etags-find-tag)
+	 ("C-." . counsel-etags-find-tag)
 	 ("M-." . counsel-etags-find-tag-at-point)
 	 ("M-r" . counsel-etags-recent-tag))
-  :config (progn
+  :init (progn
 	    (setq tags-revert-without-query t)
 	    (setq large-file-warning-threshold nil)
 	    (add-hook 'prog-mode-hook
