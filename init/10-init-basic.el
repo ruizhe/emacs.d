b9e3e03b954382eef6c84cdd3ade7f405ef9dde1
Ruizhe Zhao <ruizhe.zhao.g@gmail.com>
2019-07-19 03:32:00 -0400

    fix: disable menu always.

--- init/10-init-basic.el
+++ init/10-init-basic.el
@@ -12,9 +12,8 @@
 (when (display-graphic-p)
   (tool-bar-mode 0))
 
-;; hide menubar when work in console
-(unless window-system
-  (menu-bar-mode -1))
+;; hide menubar
+(menu-bar-mode -1)
 
 ;; show (line, column) at mode-line
 (column-number-mode t)
